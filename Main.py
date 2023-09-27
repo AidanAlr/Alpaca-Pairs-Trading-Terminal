37f451f1610432074aff114b73b428105aafd1b6
Aidan Alrawi <112656616+AidanAlr@users.noreply.github.com>
2023-09-27 01:03:14 -0400

    Delete Main.py

--- Main.py
+++
@@ -1,7 +0,0 @@
-from data_input import input_stocks, collect_data, combine_excel_files
-from data_analysis import run_correlation
-
-input_stocks()
-collect_data()
-combine_excel_files()
-run_correlation()
