834b9735ca032e9418e4f19091161ca2087d6092
Aidan Alrawi <112656616+AidanAlr@users.noreply.github.com>
2023-12-27 22:20:54 +0000

    Cleaning funcs in Visualisation.py and StatisticalMethods.py
    Signed-off-by: Aidan Alrawi <112656616+AidanAlr@users.noreply.github.com>

--- Analysis/Visualisation.py
+++ Analysis/Visualisation.py
@@ -6,19 +6,26 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(current_dir)
 # Append the root directory to sys.path so that modules can be imported
 sys.path.append(root_dir)
+
 import numpy as np
 from matplotlib import pyplot as plt
+from Analysis.StatisticalMethods import classify_zscore, collect_metrics_for_pair
 
 
-ax = ('TECK', 'WPM')[['TECK', 'WPM']].plot(
-    figsize=(12, 6))  # , title = 'Daily Closing Prices for {} and {}'.format(asset1,asset2))
-ax.set_ylabel("Closing Price")
-ax.grid(True);
+def get_tickers_from_collected_data_df(df):
+    tickers = []
+    for column in df.columns:
+        if column.endswith('_forward_return'):
+            tickers.append(column.split('_')[0])
+    return tickers[0], tickers[1]
 
 
 def spread_visualisation(df):
-    plt.title(f'stock1-{round(df.hedge_ratio[-1], 2)}*stock2')
+    stock_1, stock_2 = get_tickers_from_collected_data_df(df)
+    hedge_ratio = round(df['hedge_ratio'].iloc[-1], 2)
+    plt.title(f'{stock_1}-{hedge_ratio}*{stock_2}')
     df['spread'].plot(figsize=(16, 4), color='red')
+    plt.show()
 
 
 def zscored_spread(df):
@@ -26,19 +33,33 @@ def zscored_spread(df):
     plt.title('Zscored Spread')
     plt.axhline(1, color='k')
     plt.axhline(-1, color='k')
+    plt.show()
 
 
 def visualise_returns(df, tp, sl):
+    stock_1, stock_2 = get_tickers_from_collected_data_df(df)
     df = df.dropna()
-    df['combined_return'] = df[f'stock1_return'] + df[f'stock2_return']
+    df['combined_return'] = df[f'{stock_1}_return'] + df[f'{stock_2}_return'] * df['hedge_ratio']
+
+    def check_strategy_signal(df):
+        if df['combined_return'] > tp or df['z_score'] < -1:
+            return 1
+        elif df['combined_return'] < sl or df['z_score'] > 1:
+            return -1
+        else:
+            return 0
+
     # Trading Signal
-    df['signal'] = df.apply(lambda x: 1 if ((x['zscored'] < -1) or (x['combined_return'] > tp))
-    else (-1 if ((x['zscored'] > 1) or (x['combined_return'] < sl)) else 0), axis=1)
-    df['strategy_return'] = df[f'stock1_forward_return'] * df['signal'] + \
-                            df[f'stock2_forward_return'] * df['signal'] * -df['hedge_ratio']
+    df['signal'] = df.apply(check_strategy_signal, axis=1)
+    df['strategy_return'] = df[f'{stock_1}_forward_return'] * df['signal'] + \
+                            df[f'{stock_2}_forward_return'] * df['signal'] * -df['hedge_ratio']
 
     portfolios_cumulative_return = np.exp(np.log1p(df['strategy_return']).cumsum())
     portfolios_cumulative_return.plot(figsize=(16, 6), color='red')
     plt.title('Strategy Cumulative Returns')
     plt.ylabel('Return')
     plt.show()
+
+
+df_1 = collect_metrics_for_pair('AAPL', 'MSFT')
+visualise_returns(df_1, 0.05, -0.05)
